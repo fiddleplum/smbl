e72a0267e5e10a472b59627b29385070a8656dfc
Stephen Hurley <stephenhurley@gmail.com>
2018-05-25 16:37:30 -0700

    Added escaping to messages.

--- index.js
+++ index.js
@@ -55,18 +55,44 @@ class App {
 		symbol.innerHTML = 'sss';
 		App.symbols.push(new Placeholder(symbol));
 
-		document.getElementById('input').focus();
+		// Initialize the input element.
+		App.input = document.getElementById('input');
+		App.input.addEventListener('keyup', (event) => {
+			event.preventDefault();
+			if (event.keyCode === 13) {
+				App.setSymbol(App.input.value);
+			}
+		});
+		App.input.focus();
 
 		App.message('Smbl has been initialized.');
 	}
 
+	static setSymbol(text) {
+		App.message('Adding ' + text);
+	}
+
 	static message(text) {
-		document.getElementById('message').innerHTML = text;
+		let escapedText = text.replace(/[&<>"'/]/g, (s) => {
+			var entityMap = {
+				'&': '&amp;',
+				'<': '&lt;',
+				'>': '&gt;',
+				'"': '&quot;',
+				'\'': '&#39;',
+				'/': '&#x2F;'
+			};
+
+			return entityMap[s] || s;
+		});
+		document.getElementById('message').innerHTML = escapedText;
 	}
 }
 
 App.variableNames = [];
 App.symbols = [];
+App.input = null;
+App.activeSymbol = null;
 
 document.addEventListener('DOMContentLoaded', () => {
 	App.initialize();
